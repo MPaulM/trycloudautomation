565999b9148a1d3d15d3b4c9f15ef64084f9e631
Paul Morgan <psamoylov@gmail.com>
2021-01-31 01:28:57 -0500

    set up done

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -1,16 +1,29 @@
 package com.trycloud.tests.base;
 
+import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public abstract class TestBase {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUpMethod() {
-
+    public void setUpMethod() throws IOException {
+        driver = WebDriverFactory.getDriver("chrome");
+//        Properties properties = new Properties();
+//        String path = "configuration.properties";
+//        FileInputStream file = new FileInputStream(path);
+//        properties.load(file);
+//        driver = WebDriverFactory.getDriver(properties.getProperty("browser"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
--- src/test/java/com/trycloud/utilities/WebDriverFactory.java
+++ src/test/java/com/trycloud/utilities/WebDriverFactory.java
@@ -1,4 +1,25 @@
 package com.trycloud.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
 public class WebDriverFactory {
+    public static WebDriver getDriver (String browserType) {
+        if(browserType.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if (browserType.equalsIgnoreCase("opera")) {
+            WebDriverManager.operadriver().setup();
+            return new OperaDriver();
+        } else {
+            System.out.println("Given browser type does not exist. Driver equals null!");
+            return null;
+        }
+    }
 }
