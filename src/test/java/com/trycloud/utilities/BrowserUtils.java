836bb9ff6a022d2dc8c5c8aa7c1e2aa901c94693
Paul Morgan <psamoylov@gmail.com>
2021-01-31 00:46:19 -0500

    browser utils

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,5 +1,13 @@
 package com.trycloud.utilities;
 
 public class BrowserUtils {
+    public static void sleep(int seconds) {
+        seconds *= 1000;
+        try {
+            Thread.sleep(seconds);
+        } catch (InterruptedException e){
+            System.out.println("Something has happened in sleep method");
+        }
+    }
 
 }
