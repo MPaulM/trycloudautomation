95b77c2932ee536aca32e6f4f129342a42532507
Paul Morgan <psamoylov@gmail.com>
2021-01-31 18:25:41 -0500

    user story 01 negative case

--- src/test/java/com/trycloud/tests/US01.java
+++ src/test/java/com/trycloud/tests/US01.java
@@ -5,6 +5,8 @@ import com.trycloud.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -44,6 +46,13 @@ public class US01 {
         password.sendKeys("password");
 
         driver.findElement(By.id("submit-form")).click();
+
+        WebElement confirmationNote = driver.findElement(By.xpath("//"));
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        driver.close();
 
     }
 }
