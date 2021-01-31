c64620ca465be1127225def25e37b263aba8c6a0
Paul Morgan <psamoylov@gmail.com>
2021-01-31 00:52:00 -0500

    WebDriverFactory

--- src/test/java/com/trycloud/utilities/ConfigurationReader.java
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -1,4 +1,21 @@
 package com.trycloud.utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
+    public static Properties properties = new Properties();
+    static {
+        try {
+            FileInputStream file = new FileInputStream("configuration.properties");
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperties (String keyWord) {
+        return properties.getProperty(keyWord);
+    }
 }
