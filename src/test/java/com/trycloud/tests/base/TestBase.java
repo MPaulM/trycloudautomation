dcd0ff3e1fc5470278a2cdf37954ab572bfdd382
Paul Morgan <psamoylov@gmail.com>
2021-01-31 00:42:53 -0500

    test base

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -1,5 +1,21 @@
 package com.trycloud.tests.base;
 
-public class TestBase {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        driver.close();
+    }
 
 }
